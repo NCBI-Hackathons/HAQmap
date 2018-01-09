fda4fccfdbdfb1688188c53e044138d1d83e4ecc
Lisa Federer <lisa.federer@nih.gov>
2018-01-09 11:58:06 -0500

    add additional directions

--- SlackBuilder/ui.R
+++ SlackBuilder/ui.R
@@ -4,9 +4,13 @@ ui <- fluidPage(
   titlePanel("Slack Channel Builder"),
   
   #description
-  wellPanel("This app builds one Slack channel for each hackathon team and invites all the team members. To use this app you need two things:",
+  wellPanel("This app builds one Slack channel for each hackathon team and invites all the team members. Please read all directions below to ensure the app works for you.",
             tags$br(),
-            tags$li("A Google Sheet containing the email addresses of all your participants and the name of the team they will be participating in. These columns must be called email and team (all lower case)."),
+            tags$br(),
+            tags$b("To use this app you need two things:"),
+            
+            tags$br(),
+            tags$li("A Google Sheet containing the email addresses of all your participants and the name of the team they will be participating in. These columns must be called email and team (all lower case). Important note: this Sheet must be (at least temporarily while you run this app) made public by completing two steps. First, share the Sheet publicly (see Step 2, Share a file publicly, at https://support.google.com/drive/answer/2494822). Next, select File >Publish to the web and click the Publish button. You can unshare/unpublish your sheet immediately after running this app."),
             tags$li("A Slack workspace. First, create your Slack workspace (directions at https://get.slack.help/hc/en-us/articles/206845317-Create-a-Slack-workspace). Next, get the API token for your workspace from https://api.slack.com/custom-integrations/legacy-tokens.")
             ),
   
